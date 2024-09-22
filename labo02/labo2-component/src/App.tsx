891e37524fc3896fecf7ad067912917236958317
Yazan <yazan.dukhan@student.ap.be>
2024-09-22 07:23:02 +0200

    1. Simpele componenten Done!

--- labo02/labo2-component/src/App.tsx
+++ labo02/labo2-component/src/App.tsx
@@ -1,34 +1,64 @@
+import React from "react"
+
 interface HeaderProps {
   title: string
   subtitle: string
 }
 const Header = ({ title, subtitle }: HeaderProps) => {
   return (
-    <>
+    <React.Fragment>
       <h1>{title}</h1>
       <h2>{subtitle}</h2>
-    </>
+    </React.Fragment>
+  )
+}
+
+
+interface ListItemProps {
+  text : string
+}
+const ListItem = ({text} : ListItemProps) => {
+  return (
+    <React.Fragment>
+    <li key={text}>{text}</li>
+    </React.Fragment>
   )
 }
 
 interface ListProps {
   items : string[]
 }
-
 const List = ({items} : ListProps) => {
   return (
-    <>
-    //{items.map(item =>item )}
-    </>
+    <React.Fragment>
+    <ul>
+    {items.map(item =><ListItem key={item} text={item}></ListItem>)}
+    </ul>
+    </React.Fragment>
+  )
+}
+
+interface FooterProps{
+  copy : string
+  year : number
+}
+const Footer = ({copy, year} : FooterProps) => {
+  return(
+    <React.Fragment>
+      <p>© {copy} ({year})</p>
+    </React.Fragment>
   )
 }
 
 
 const App = () => {
+  const items : string[] = ["item 1","item 2","item 3"]
   return (
-    <>
+    <React.Fragment>
       <Header title="Labo 2" subtitle="Basic components"></Header>
-    </>
+      <List items={items}></List>
+      <Footer copy="Yazan Dukhan" year={2024}></Footer>
+    </React.Fragment>
   );
 };
 
