122fc0e450fc483945d49f0e8f27d1774db8c948
Yazan <yazan.dukhan@student.ap.be>
2024-09-28 18:28:43 +0200

    labo2-maaltafels DONE!

--- labo02/labo2-maaltafels/src/App.tsx
+++ labo02/labo2-maaltafels/src/App.tsx
@@ -1,33 +1,62 @@
-// import React from "react";
+import './index.css'
 
-const rij: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const kolom: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+interface MultiplicationRowProps {
+  factor: number
+  max: number
+}
+const MultiplicationRow = ({ factor, max }: MultiplicationRowProps) => {
+  return (
+    <>
+      <tr>
+        <td><strong>{factor}</strong></td>
+        {[...Array(max).keys()].map(i =>
+          <td key={i}>{factor * (i + 1)}</td>
+        )}
+      </tr>
+    </>
+  )
+}
 
-// interface MultiplicationTableProps{
-//   max :  number
-// }
-// const MultiplicationTable = ({max} : MultiplicationTableProps) => {
-//   return(
-//     <>
-    
-//     </>
-//   )
-// }
+interface MultiplicationTableProps {
+  max: number
+}
+const MultiplicationTable = ({ max }: MultiplicationTableProps) => {
+  return (
+    <table>
+      <thead>
+        <Header max={max} />
+      </thead>
+      <tbody>
+        {[...Array(max).keys()].map(i => (
+          <MultiplicationRow key={i} factor={i + 1} max={max} />
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+interface HeaderProps {
+  max: number
+}
+const Header = ({ max }: HeaderProps) => {
+  return (
+    <>
+      <th></th> {/* Lege cel voor de linkerkant */}
+      {[...Array(max).keys()].map(i => (
+        <th key={i}>{i + 1}</th>
+      ))}
+    </>
+  )
+}
 
 const App = () => {
 
   return (
     <>
-      <table>
-        {kolom.map(y =>
-          <tr>
-            {rij.map(x =>
-              <td>{y*x}</td>
-            )}
-          </tr>)
-        }
-
-      </table>
+      <h1>Maaltafels</h1>
+      <MultiplicationTable max={5} />
+      <MultiplicationTable max={10} />
+      <MultiplicationTable max={15} />
     </>
   );
 };
