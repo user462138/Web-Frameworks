5dc9b0309ac47eabef9b65e4c2c74202fb585d7e
Yazan <yazan.dukhan@student.ap.be>
2024-09-29 10:01:15 +0200

    labo4-loading DONE!

--- labo04/labo4-loading/src/App.tsx
+++ labo04/labo4-loading/src/App.tsx
@@ -1,13 +1,34 @@
+import { ColorRing } from 'react-loader-spinner'
 import './App.css'
+import { useState } from 'react'
 
-
-const App = () => {
-
+const Loading = () => {
   return (
     <>
-    <center>
-      <button>Start loading</button>
-    </center>
+      <ColorRing
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="color-ring-loading"
+        wrapperStyle={{}}
+        wrapperClass="color-ring-wrapper"
+        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+      />
+    </>
+  )
+}
+
+const App = () => {
+  const [loading, setLoading] = useState(false)
+  const startLoading = () => {
+    setLoading(true)
+    setTimeout(() => {setLoading(false)}, 3000)
+  }
+  return (
+    <>
+      <center>
+        {loading ? <Loading></Loading> : <button onClick={startLoading}>Start loading</button>}
+      </center>
     </>
   )
 }
