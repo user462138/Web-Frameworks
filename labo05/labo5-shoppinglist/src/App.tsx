250e572a0dd0a87d3c55b7dc4f6162f91395003c
Yazan <yazan.dukhan@student.ap.be>
2024-09-30 13:57:01 +0200

    labo5-shopinglist DONE!

--- labo05/labo5-shoppinglist/src/App.tsx
+++ labo05/labo5-shoppinglist/src/App.tsx
@@ -11,15 +11,53 @@ const ShoppingList = () => {
   const [shoppingList, setShoppingList] = useState<Item[]>([])
   const [name, setName] = useState("");
   const [quantity, setQuantity] = useState(0);
+  const [confirmationText, setConfirmationText] = useState("")
+  const [errorText, setErrorText] = useState("")
+
 
   const addIetm: React.MouseEventHandler<HTMLButtonElement> = () => {
-    setShoppingList([...shoppingList, { name: name, quantity: quantity }]);
+    if (!name) {
+      setErrorText(" Name must not be empty")
+      setConfirmationText("")
+    }
+    if (quantity <= 0) {
+      setErrorText(" Quantity must be greater than 0")
+      setConfirmationText("")
+    } if (name && quantity > 0) {
+      setShoppingList([...shoppingList, { name: name, quantity: quantity }])
+      setConfirmationText(`Item added to shopping list.`)
+      setErrorText("")
+    }
   }
 
-  // const addItemHandeler = 
+  const removeIetm = (name : string) => {
+    let numbersCpy = shoppingList.filter((Item) => Item.name !== name);
+    setShoppingList(numbersCpy);
+    setConfirmationText(`Item removed from shopping list.`)
+  }
   return (
     <>
-      <div style={{width: '500px'}}>
+      <div style={{ width: '500px' }}>
+        {confirmationText && <div
+          style={{
+            backgroundColor: " #dff0d8",
+            border: "1px solid #d6e9c6",
+            borderRadius: "4px",
+            color: " #3c763d",
+            marginBottom: "10px",
+            padding: "10px"
+          }}
+        > {confirmationText} </div>}
+        {errorText && <div
+          style={{
+            backgroundColor: "#f2dede",
+            border: "1px solid #ebccd1",
+            borderRadius: "4px",
+            color: " #a94442",
+            marginBottom: "10px",
+            padding: "10px"
+          }}
+        > {errorText} </div>}
         <div style={{ display: 'grid', gridTemplateColumns: '100px 1fr' }}>
           <label>Name:</label>
           <input type="text" placeholder="Name" value={name} onChange={(event) => setName(event.target.value)} />
@@ -30,24 +68,24 @@ const ShoppingList = () => {
         <button type="button" onClick={addIetm}> Add </button>
         <br />
         <table>
-        <thead>
-          <tr>
-            <th style={{ width: "400px" }}>Name</th>
-            <th>Quantity</th>
-            <th style={{ width: "100px" }}></th>
-          </tr>
-        </thead>
-        <tbody>
-          {shoppingList.map(x =>
+          <thead>
             <tr>
-              <td>{x.name}</td>
-              <td>{x.quantity}</td>
-              <td><input type="button" value="Remove" /></td>
+              <th style={{ width: "400px" }}>Name</th>
+              <th>Quantity</th>
+              <th style={{ width: "100px" }}></th>
             </tr>
-          )}
-        </tbody>
-      </table>
-      </div>
+          </thead>
+          <tbody>
+            {shoppingList.map(x =>
+              <tr>
+                <td>{x.name}</td>
+                <td>{x.quantity}</td>
+                <td><input value="Remove" type="button" onClick={_e => removeIetm(x.name)}  /></td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div >
     </>
   )
 }
