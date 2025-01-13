7930c267c02557e1209e1704fb3ec6c19e34dbda
Yazan <yazan.dukhan@student.ap.be>
2025-01-13 14:45:40 +0100

    Animals API routing DONE!

---
+++ labo11/animals-api-routing/src/pages/animals/[name].tsx
@@ -0,0 +1,34 @@
+import { useRouter } from "next/router";
+import animalsJson from "@/animals.json"
+import { useEffect, useState } from "react";
+import { Animal } from "@/types";
+ 
+const Animasl = () => {
+  const router = useRouter()
+  const {name} = router.query
+  const [animal, setAnimal] = useState<Animal>();
+  const [loading, setLoading] = useState(true)
+  // const animals =  animalsJson.animals;
+  // const animal = animals.find(animal => animal.name === name)
+
+  useEffect(() => {
+    if (router.isReady) {
+      fetch(`http://localhost:3000/api/animals`)
+        .then(response => response.json())
+        .then((animals:Animal[]) => {
+          const animal = animals.find(animal => animal.name === name);
+          setAnimal(animal)
+          setLoading(false)
+        })
+    }
+  }, [router.isReady, name])
+
+  if (loading) return <p>Loading...</p>
+  if (animal) {
+    return <p>Animasl:  {animal.name} <img src={animal.img} alt="" /></p>
+  } else {
+    return <p>Animal was not found</p>
+  }
+}
+
+export default Animasl;
